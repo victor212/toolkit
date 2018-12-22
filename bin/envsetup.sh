9baac67e0f108cf49c11b6e1e187622155b7e9b3
victor.d <victor.d@foxmail.com>
2018-12-22 16:51:37 +0800

    add some help informations

--- bin/envsetup.sh
+++ bin/envsetup.sh
@@ -1,12 +1,51 @@
 #!/bin/bash
 
-function sos()
+function help()
 {
-	echo "This is my help sos"
+	echo "sos: options"
+	echo " 	-h help"
+	echo " 	tips"
+	echo " 	markcs"
+	echo " 	clearcs"
+	echo " 	mkcscopefiles"
+	echo " 	mktags"
+
+}
+
+function common_tips()
+{
+	echo "This is my common tips"
 	echo "ctags -R"
 	echo "  :set tags=xxx"
 	echo "cscope -Rbkq"
 	echo "  :cs add cscope.out"
+
+}
+
+function sos()
+{
+	# Need to clear builtin variables first
+	OPTERR=""
+	OPTIND=0
+	OPTARG=""
+	while getopts :h OPTION; do
+		case $OPTION in
+		h) 	#echo "optarg = " $OPTARG
+			#echo "optind = " $OPTIND
+			help
+			;;
+		?) 	echo "Invalid parameters"
+			help
+		   	exit 1;;
+		esac
+	done
+# 	echo "optind = "$OPTIND
+
+	# Keep rest parameters only 
+	shift $(($OPTIND - 1))
+
+	# The no leading opt parameters
+	echo $1
 
 }
 
